479e45b3dcf94565f707c74f4e8e32ac2e5d2e20
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-08-01 21:40:14 -0500

    Update variable.tf

--- aws/variable.tf
+++ aws/variable.tf
@@ -1,1 +1,24 @@
 
+# AWS settings
+
+variable "region" {
+	type      = "string"
+    default = ""
+}
+
+variable "account_id" {
+    type    = "string"
+    default = ""
+}
+
+# Facebook integration settings
+
+variable "facebook_verify_token" {
+	type    = "string"
+  default = ""
+}
+
+variable "facebook_page_token" {
+    type    = "string"
+    default = ""
+}
